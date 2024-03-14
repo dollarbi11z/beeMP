ade93f028f719aadb85b260f1f20dd4af54a6e9a
wav£d <proplaya66789@gmail.com>
2024-03-14 17:42:23 +0000

    Ready for use.

--- beeMP.cpp
+++ beeMP.cpp
@@ -3,33 +3,19 @@
 
 bool beeMP::_flgFlipVert = false;
 
-uint8_t	 beeMP::_flgEndian { 0 };
-uint32_t beeMP::_rowStride { 0 };
-
 inline void beeMP::set_flip_image_vert()
 {
 	_flgFlipVert = true;
 }
 
-inline void beeMP::error_log(const char* message)
+inline void beeMP::error_log(const uint8_t* message)
 { 
 	std::cout << "[beeMP] [ERROR] " << message << std::endl;
 }
 
-inline void beeMP::debug_log( const char* message )
+inline void beeMP::debug_log( const uint8_t* message )
 {
 	std::cout << "[beeMP] [DEBUG] " << message << std::endl;
-}
-
-uint32_t beeMP::align_stride( uint32_t stride )
-{
-	uint32_t newStride = _rowStride;
-
-	while( newStride % stride != 0 )
-	{
-		newStride++;
-	}
-	return newStride;
 }
 
 bool beeMP::sanitise_header_file( BITMAPFILEHEADER* header )
@@ -45,41 +31,41 @@ bool beeMP::sanitise_header_file( BITMAPFILEHEADER* header )
 	return true;
 }
 
-bool beeMP::sanitise_info_header_24( BITMAPINFOHEADER* header )
+bool beeMP::sanitise_info_header( BITMAPINFOHEADER* header )
 {
 	if( header->biSize != 40 )
 	{
-		error_log( "** INFO HEADER IS INVALID SIZE (EXPECTED: 40-BYTES) **" );
+		error_log( "INFO HEADER IS INVALID SIZE (EXPECTED: 40-BYTES)" );
 		return false;
 	}
 
 	if( header->biBitCount != 24)
 	{
-		error_log( "** INVALID BIT COUNT (EXPECTED: 24) **" );
+		error_log( "INVALID BIT COUNT (EXPECTED: 24)" );
 		return false;
 	}
 
 	if( header->biCompression != 0 )
 	{
-		error_log( "** INVALID COMPRESSION VALUE (EXPECTED: 0) **" );
+		error_log( "INVALID COMPRESSION VALUE (EXPECTED: 0)" );
 		return false;
 	}
 
 	if( header->biHeight < 0 )
 	{
-		error_log( "** INVALID HEIGHT VALUE (TOP-DOWN FILES ARE NOT SUPPORTED YET) ** " );
+		error_log( "INVALID HEIGHT VALUE (TOP-DOWN FILES ARE NOT SUPPORTED YET)" );
 		return false;
 	}
 
 	if( header->biPlanes != 1 )
 	{
-		error_log( "** INVALID COLOUR PLANE VALUE (EXPECTED: 1) **" );
+		error_log( "INVALID COLOUR PLANE VALUE (EXPECTED: 1)" );
 		return false;
 	}
 
 	if( header->biClrUsed != 0 )
 	{
-		error_log( " ** INVALID COLOUR AMOUNT (24-BIT FILES DON'T HAVE A PALETTE) **" );
+		error_log( "INVALID COLOUR AMOUNT (24-BIT FILES DON'T HAVE A PALETTE)" );
 		return false;
 	}
 
@@ -87,47 +73,23 @@ bool beeMP::sanitise_info_header_24( BITMAPINFOHEADER* header )
 	return true;
 }
 
-bool beeMP::sanitise_info_header_32( BITMAPV4HEADER* header )
+inline int32_t beeMP::get_padding_size( BITMAPINFOHEADER& header )
 {
-	return false;
+	return ((header.biWidth * header.biBitCount + 31) / 32) * 4;
 }
 
-uint8_t* beeMP::get_colour_palette_32( BITMAPINFOHEADER* info, std::ifstream& stream )
+uint8_t* beeMP::load_bitmap( const char* file, uint32_t& width, uint32_t& height )
 {
-	uint8_t* palette32;
+	BITMAPFILEHEADER		bmpHeader;
+	BITMAPINFOHEADER		bmpInfo;
 
-	debug_log( "** RETRIEVED COLOUR PALETTE **" );
-	return nullptr;
-}
-
-uint8_t* beeMP::load_bitmap( const char* file, uint32_t& width, uint32_t& height, bool hasAlpha )
-{
 	uint8_t* ret = nullptr;
-
-	switch( hasAlpha )
-	{
-		case true:
-			ret = load_bitmap_32( file, width, height );
-			return ret;
-		case false:
-			ret = load_bitmap_24( file, width, height );
-			return ret;
-	}
-	return ret;
-}
-
-uint8_t* beeMP::load_bitmap_24( const char* file, uint32_t& width, uint32_t& height )
-{
-	BITMAPFILEHEADER bmpHeader;
-	BITMAPINFOHEADER bmpInfo;
 
 	debug_log( "** LOADING 24-BIT IMAGE **" );
 
-	// Store the image data.
-	std::vector<uint8_t> _imageData;
-
 	// ifstream to read image.
 	std::ifstream _imageFile( file, std::ios::binary );
+	std::vector<uint8_t> data;
 
 	// Ifstream object with open mode set to 'Binary'.
 	_imageFile.exceptions( std::ios::badbit | std::ios::failbit );
@@ -135,7 +97,7 @@ uint8_t* beeMP::load_bitmap_24( const char* file, uint32_t& width, uint32_t& hei
 	if( !_imageFile )
 	{
 		error_log( "** UNABLE TO OPEN IMAGE-FILE **" );
-		return nullptr;
+		return {};
 	}
 
 	// Read in BITMAPFILEHEADER data
@@ -144,107 +106,50 @@ uint8_t* beeMP::load_bitmap_24( const char* file, uint32_t& width, uint32_t& hei
 	if( sanitise_header_file( &bmpHeader ) != true )
 	{
 		error_log( "** FAILED TO VERIFY FILE HEADER **" );
-		return nullptr;
+		return {};
 	}
 
 	// Read in BITMAPINFOHEADER data
 	_imageFile.read( (char*) &bmpInfo, sizeof( BITMAPINFOHEADER ) );
 
-	if( sanitise_info_header_24( &bmpInfo ) != true )
+	if( sanitise_info_header( &bmpInfo ) != true )
 	{
 		error_log( "** FAILED TO VERIFY INFO HEADER **" );
-		return nullptr;
+		return {};
+	}
+
+	int32_t paddingRow = get_padding_size( bmpInfo );
+	int32_t imageSize = bmpInfo.biHeight * paddingRow;
+
+	_imageFile.seekg( bmpHeader.bfOffsetData, std::ios::beg );
+	data.resize( imageSize );
+
+	_imageFile.read( (char*)data.data(), data.size() );
+	data.reserve( (bmpInfo.biWidth * bmpInfo.biHeight * bmpInfo.biBitCount) / 8 );
+
+	// Fill image data 
+	for( int32_t i = 0; i < bmpInfo.biHeight; i++ )
+	{
+		int32_t k = (bmpInfo.biHeight - 1) - i;
+		uint8_t* ptr = (uint8_t*)data.data() + k * paddingRow;
+		data.insert( data.end(), ptr, ptr + bmpInfo.biWidth * 3 );
+	}
+
+	// Swap BGR to RGB
+	uint8_t bufRGB = 0; 
+	for( uint64_t i = 0; i < bmpInfo.biSizeImage; i += 3 )
+	{
+		bufRGB		= data[i];
+		data[i]		= data[i + 2];
+		data[i + 2] = bufRGB;
 	}
 
 	width = bmpInfo.biWidth;
 	height = bmpInfo.biHeight;
 
-	uint32_t dataSize = (bmpInfo.biBitCount * bmpInfo.biWidth + 31 / 32) * 4;
-	_imageData.resize( dataSize );
-
-	// Read image data
-	_imageFile.read( (char*) _imageData.data(), dataSize);
-
-	// Swap B and R channels
-	for( uint32_t i = 0; i < dataSize; i += 3 )
-	{
-		uint8_t bufRGB		= _imageData[i];
-		_imageData[i]		= _imageData[i + 2];
-		_imageData[i + 2]	= bufRGB;
-	}
+	ret = new uint8_t[data.size()];
+	std::memcpy( ret, data.data(), data.size() );
 
 	debug_log( "** LOADED IMAGE **" );
-
-	return _imageData.data();
-}
-
-uint8_t* beeMP::load_bitmap_32( const char* file, uint32_t& width, uint32_t& height )
-{
-	BITMAPFILEHEADER		bmpHeader;
-	BITMAPV4HEADER			bmpInfo;
-
-	debug_log( "**	LOADING 32-BIT IMAGE  **" );
-
-	// Store the image data.
-	std::vector<uint8_t> _imageData;
-
-	// ifstream to read image.
-	std::ifstream _imageFile( file, std::ios::binary );
-
-	// Ifstream object with open mode set to 'Binary'.
-	_imageFile.exceptions( std::ios::badbit | std::ios::failbit );
-
-	if( !_imageFile )
-	{
-		error_log( "** UNABLE TO OPEN IMAGE-FILE **" );
-		return nullptr;
-	}
-
-	// Read in BITMAPFILEHEADER data
-	_imageFile.read( (char*) &bmpHeader, sizeof( BITMAPFILEHEADER ) );
-
-	if( sanitise_header_file( &bmpHeader ) != true )
-	{
-		error_log( "** FAILED TO VERIFY FILE HEADER **" );
-		return nullptr;
-	}
-
-	// Read in BITMAPINFOHEADER data
-	_imageFile.read( (char*) &bmpInfo, sizeof( BITMAPINFOHEADER ) );
-
-	if( sanitise_info_header_32( &bmpInfo ) != true )
-	{
-		error_log( "** FAILED TO VERIFY INFO HEADER **" );
-		return nullptr;
-	}
-
-	bmpInfo.bV4Size = sizeof( BITMAPFILEHEADER ) + sizeof( BITMAPV4HEADER );
-	bmpHeader.bfOffsetData = sizeof( BITMAPFILEHEADER ) + sizeof( BITMAPV4HEADER );
-
-	// Calculate the size of the data array
-	uint32_t dataSize = bmpInfo.bV4Width * bmpInfo.bV4Height * bmpInfo.bV4BitCount / 8;
-	_imageData.resize( dataSize );
-
-	if( bmpInfo.bV4BitCount % 4 == 0 )
-	{
-		_imageFile.read( reinterpret_cast<char*>(_imageData.data()), dataSize );
-		bmpHeader.bfSize += dataSize;
-	}
-	else
-	{
-		_rowStride = bmpInfo.bV4Width * bmpInfo.bV4BitCount / 8;
-		uint32_t newStride = align_stride( 4 );
-
-		std::vector<uint8_t> padding_row( newStride - _rowStride );
-
-		for( int32_t y = 0; y < bmpInfo.bV4Height; y++ )
-		{
-			_imageFile.read( (char*) (_imageData.data() + _rowStride * y), _rowStride );
-			_imageFile.read( (char*) padding_row.data(), padding_row.size() );
-		}
-		bmpHeader.bfSize += dataSize + bmpInfo.bV4Height * static_cast<uint32_t>(padding_row.size());
-	}
-
-	debug_log( "** LOADED IMAGE **" );
-	return _imageData.data();
+	return ret;
 }
--- beeMP.h
+++ beeMP.h
@@ -9,11 +9,26 @@
 											\_______/  \_______| \_______|\__|     \__|\__|      
 
 
-											TODO:
-											- Support different bit depths (2, 4, 8, 16, 32).
-											- Ensure safety of code.
-											- Optimise performance.
-											- Break this alot in testing.
+											HOW TO USE:				
+											Create two unsigned ints in your class to store dimensions
+
+											Create a vector<uint8_t> in your class to store the data.
+												-> I will fix this later on.
+
+											Use beeMP::load() and pass the texture location and 
+											variables in.
+
+										    There is also a boolean value for if the image has alpha
+											channels and this will determine the image to be 32-bit.
+
+											load() will fill the input variables with the data read
+											from the bitmap file.
+
+											From there, you now have the necessary image data to map
+											to an OpenGL texture or related uses.
+
+											I may create a custom struct to contain all of the image
+											data so the user can extract the data manually.
 										
 */
 
@@ -23,9 +38,7 @@
 #define WIN32_LEAN_AND_MEAN 
 
 #define BITMAP_MAGIC_IDENTIFIER	0x4D42
-#define SRGB_COLOUR_SPACE		0x73524742
 
-#include <unordered_map>
 #include <iostream>
 #include <cstdint>
 #include <vector>
@@ -46,7 +59,7 @@ typedef struct tagBITMAPINFOHEADER
 {
 	uint32_t    biSize;          // Size of the header (should be 40)
 	uint32_t    biWidth;         // Image width in pixels
-	uint32_t    biHeight;        // Image height in pixels
+	int32_t     biHeight;        // Image height in pixels
 	uint16_t    biPlanes;        // Number of color planes (must be 1)
 	uint16_t    biBitCount;      // Number of bits per pixel
 	uint32_t    biCompression;   // Compression method (0 for uncompressed)
@@ -57,57 +70,22 @@ typedef struct tagBITMAPINFOHEADER
 	uint32_t    biClrImportant;  // Number of important colors (usually 0)
 } BITMAPINFOHEADER;
 
-typedef struct tagBITMAPV4HEADER
-{
-	uint32_t bV4Size;
-	int32_t  bV4Width;
-	int32_t  bV4Height;
-	uint16_t bV4Planes;
-	uint16_t bV4BitCount;
-	uint32_t bV4V4Compression;
-	uint32_t bV4SizeImage;
-	int32_t  bV4XPelsPerMeter;
-	int32_t  bV4YPelsPerMeter;
-	uint32_t bV4ClrUsed;
-	uint32_t bV4ClrImportant;
-	uint32_t bV4RedMask;
-	uint32_t bV4GreenMask;
-	uint32_t bV4BlueMask;
-	uint32_t bV4AlphaMask;
-	uint32_t bV4CSType;
-	int32_t  bV4Endpoints[9];
-	uint32_t bV4GammaRed;
-	uint32_t bV4GammaGreen;
-	uint32_t bV4GammaBlue;
-} BITMAPV4HEADER;
-
-/* A (no-longer) fast, (no-longer) simple, (hopefully) safe static library for loading 24-bit & 32-bit bitmap files. */
+/* A fast, (hopefully) safe static library for loading 24-bit bitmap files. */
 class beeMP
 {
 private:
-	static uint32_t             _rowStride;		
-
-	static uint8_t              _flgEndian;		/* 1u - Little Endian | 2u - Big Endian */ 
 	static bool                 _flgFlipVert;	/* Will the image be flipped vertically (W.I.P) */
 
-	inline static void error_log( const char* message); /* Use this to log errors and display to console */
-	inline static void debug_log( const char* message ); /* Use this to debug output and display to console */
+	inline static void error_log( const uint8_t* message); /* Use this to log errors and display to console */
+	inline static void debug_log( const uint8_t* message ); /* Use this to debug output and display to console */
+
+	inline static int32_t get_padding_size( BITMAPINFOHEADER& header ); /* Returns the necessary padding */
 
 	static bool sanitise_header_file( BITMAPFILEHEADER* header );
-	static bool sanitise_info_header_24( BITMAPINFOHEADER* header );
-	static bool sanitise_info_header_32( BITMAPV4HEADER* header );
-
-	static uint32_t align_stride( uint32_t stride );
-	static uint8_t* get_colour_palette_32( BITMAPINFOHEADER* info, std::ifstream& stream );
-
-	/* BROKEN */
-	static uint8_t* load_bitmap_24( const char* file, uint32_t& width, uint32_t& height );
-
-	/* NEEDS IMPLEMENTATION */
-	static uint8_t* load_bitmap_32( const char* file, uint32_t& width, uint32_t& height );
+	static bool sanitise_info_header( BITMAPINFOHEADER* header );
 
 public:
-	static uint8_t* load_bitmap( const char* file, uint32_t& width, uint32_t& height, bool hasAlpha );
+	static uint8_t* load_bitmap( const char* file, uint32_t& width, uint32_t& height );
 
 	// TODO: implement row order flipping
 	inline static void set_flip_image_vert();
